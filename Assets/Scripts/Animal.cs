9384efa1b080a069990384fc2073127dc88b28d3
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2020-12-25 16:16:51 +0200

    Add animals classes

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Animal : IBehavier
+{
+    protected float health;
+    protected float damage;
+    protected float shield;
+
+    public void attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void move()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/IBehavior.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBehavier
+{
+    void attack();
+    void move();
+}
