b4602e9ce405c609b029d2c88cd3f6d6dd6639cf
Revertigo <dekel241@gmail.com>
2020-12-25 20:50:40 +0200

    Add battle system to spawn new animals

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYER_ONE_TURN, PLAYER_TWO_TURN, END}
+
+public class BattleSystem : MonoBehaviour
+{
+    BattleState state;
+
+    public GameObject[] playerOneArmy;
+    public GameObject[] playerTwoArmy;
+
+    public Transform[] armyOneInitialLocation;
+    public Transform[] armyTwoInitialLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetUpBattle();
+    }
+
+    void SetUpBattle()
+    {
+        //TODO: change to player's animals pick
+        Instantiate(playerOneArmy[0], armyOneInitialLocation[0]);
+        Instantiate(playerTwoArmy[0], armyTwoInitialLocation[0]);
+
+        state = BattleState.PLAYER_ONE_TURN;
+    }
+}
