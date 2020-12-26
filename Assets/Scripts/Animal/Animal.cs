6a7895f1059dc91084000cb753ed1ccdbd24de66
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2020-12-26 20:29:42 +0200

    Update scene and Animal

--- Assets/Scripts/Animal/Animal.cs
+++ Assets/Scripts/Animal/Animal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public abstract class Animal : IBehavior
+public abstract class Animal : MonoBehaviour, IBehavior
 {
     protected float currectHP;
     protected float maxHP;
@@ -13,9 +13,26 @@ public abstract class Animal : IBehavior
 	protected Animator animator;
     private float rotationDegreePerSecond = 1000;
 
+
+    [SerializeField]
+    Transform attackPos;
+
+    [SerializeField]
+    float attackRangeX;
+
+    [SerializeField]
+    float attackRangeY;
+
+    [SerializeField]
+    LayerMask whatIsEnemies;
+
     public void attack()
     {
-        
+        Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(attackPos.position, new Vector2(attackRangeX, attackRangeY), 0, whatIsEnemies);
+        for (int i = 0; i < enemiesToDamage.Length; i++)
+        {
+            //enemiesToDamage[i].GetComponent<DamageScript>().TakeDamage(animal.getDamage()); // TODO make a script to deal damage of the enemy
+        }
     }
 
     public void move(Transform transform)
@@ -38,6 +55,25 @@ public abstract class Animal : IBehavior
         }
             
         transform.position += position * Time.deltaTime * getWalkSpeed();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(attackPos.position, new Vector3(attackRangeX, attackRangeY, 0));
+    }
+
+    private void Update()
+    {
+        if (gameObject.GetComponent<MoveController>().enabled)
+        {
+            move(transform);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            attack();
+        }
     }
 
     public float getWalkSpeed()
--- Assets/Scripts/Controller/MoveController.cs
+++ Assets/Scripts/Controller/MoveController.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    [SerializeField]
-    Animal animal;
-
-    private float horizontal;
-    private float vertical;
-
-    private float rotationDegreePerSecond = 1000;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,26 +14,5 @@ public class MoveController : MonoBehaviour
     void Update()
     {
 
-    }
-
-    private void FixedUpdate()
-    {
-        /*horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        Vector3 stickDirection = new Vector3(horizontal, 0, 0);
-        Vector3 position = new Vector3(horizontal, vertical, 0);
-
-        *//*if (stickDirection.sqrMagnitude > 1)
-        {
-            stickDirection.Normalize();
-            position.Normalize();
-        }*//*
-
-        if (stickDirection != Vector3.zero)
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(stickDirection, Vector3.up), rotationDegreePerSecond * Time.deltaTime);
-        transform.position += position * Time.deltaTime * animal.getWalkSpeed();*/
-
-        animal.move(transform);
-    }
+    }   
 }
