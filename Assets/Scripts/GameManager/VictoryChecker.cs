f96751903fc252af06a468817e5dcdbb93703256
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-01-01 20:28:27 +0200

    Add victory checker

---
+++ Assets/Scripts/GameManager/VictoryChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryChecker : MonoBehaviour
+{
+    GameObject[] teamOne;
+    GameObject[] teamTwo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        teamOne = GameObject.FindGameObjectsWithTag("PlayerOne");
+        teamTwo = GameObject.FindGameObjectsWithTag("PlayerTwo");
+
+        if (teamOne.Length == 0)
+        {
+            Debug.Log("Team 2 wins");
+        }
+        else if (teamTwo.Length == 0)
+        {
+            Debug.Log("Team 1 wins");
+        }
+ 
+    }
+}
