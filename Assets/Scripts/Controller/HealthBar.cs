a38c09dffaa353bd0fd8c87eff2eab8104c7bb27
Revertigo <dekel241@gmail.com>
2021-01-01 14:34:06 +0200

    Fix reduction mechanism

--- Assets/Scripts/Controller/HealthBar.cs
+++ Assets/Scripts/Controller/HealthBar.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
-    public Transform bar;
-    readonly float maxHealth = 1f;
-    float hp;
+    Transform bar;
+    float scaleFactor = 1.0f; //Since scaling is maximum 1
+    float maxHP;
+    float hp; //Current animal's HP
 
     // Start is called before the first frame update
     public void Start()
     {
-       bar = transform.Find("Bar");
-       bar.localScale = new Vector3(maxHealth, maxHealth);
-       hp = maxHealth;
+        hp = maxHP = GetComponentInParent<Animal>().MaxHP;
+        bar = transform.Find("Bar");
+        bar.localScale = new Vector3(scaleFactor, scaleFactor);
     }
 
     public void ReduceHP(float damage)
     {
         hp -= damage;
-        bar.localScale = new Vector3(hp, maxHealth);
+        bar.localScale = new Vector3(hp / maxHP, scaleFactor);
         if (hp <= 0)
         {
             Destroy(gameObject.transform.parent.gameObject);
