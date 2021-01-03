c52f6d7e417a277c4f696aa586be6444bd144e96
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-01-03 21:08:54 +0200

    Add Map selection

---
+++ Assets/Scripts/UI/MapChoose.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapChoose : MonoBehaviour
+{
+    [SerializeField] Button[] maps = new Button[3];
+    private int currentMap;
+    readonly int lowerBoundary = -1;
+    readonly int higherBoundary = 3;
+    // Start is called before the first frame update
+    private void Start()
+	{
+        currentMap = 0;
+        transform.GetChild(currentMap).gameObject.SetActive(true);
+    }
+
+	void selectMap(int index)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == index);
+        }
+    }
+
+    public void changeMap(int change)
+    {
+        if (change + currentMap != lowerBoundary && change + currentMap != higherBoundary)
+        {
+            currentMap += change;
+            selectMap(currentMap);
+        }
+    }
+}
