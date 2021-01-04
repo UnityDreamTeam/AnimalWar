81c58d87e767358d9741078f807b7b9f4fc085a0
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2021-01-04 19:32:59 +0200

    Add function to update the hp and atk text

--- Assets/Scripts/Controller/HealthBar.cs
+++ Assets/Scripts/Controller/HealthBar.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HealthBar : MonoBehaviour
@@ -13,17 +14,31 @@ public class HealthBar : MonoBehaviour
     public void Start()
     {
         hp = maxHP = GetComponentInParent<Animal>().MaxHP;
+        setInfo();
         bar = transform.Find("Bar");
         bar.localScale = new Vector3(scaleFactor, scaleFactor);
+        
     }
 
     public void ReduceHP(float damage)
     {
         hp -= damage;
         bar.localScale = new Vector3(hp / maxHP, scaleFactor);
+        setInfo();
         if (hp <= 0)
         {
             Destroy(gameObject.transform.parent.gameObject);
         }
+    }
+
+    void setInfo()
+    {
+        transform.Find("Info").GetComponent<TextMeshPro>().text = "HP: " + hp + " atk: " + GetComponentInParent<Animal>().Damage;
+    }
+
+    private void Update()
+    {
+        //TODO fix rotation of the text
+        //transform.Find("Info").transform.Rotate(new Vector3(0, gameObject.transform.parent.rotation.y - 90, 0));
     }
 }
