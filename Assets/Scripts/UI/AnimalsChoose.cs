a632bc2185990724dbf723434ed15bb541e269a6
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2021-01-08 20:49:07 +0200

    Fix bug animal choose

--- Assets/Scripts/UI/AnimalsChoose.cs
+++ Assets/Scripts/UI/AnimalsChoose.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEditor.Events;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,48 +6,48 @@ using UnityEngine.UI;
 
 public class AnimalsChoose : MonoBehaviour
 {
-    [SerializeField] GameObject PlayerOneArmyPosition;
-    List<GameObject> playerOneArmy;
+    [SerializeField] GameObject PlayerOneArmyPosition = null;
+    GameObject[] playerOneArmy;
     readonly int maxNumberOfAnimals = 5;
-    readonly int minNumberOfAnimals = 1;
+    int countAnimals = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerOneArmy = new List<GameObject>();
+        playerOneArmy = new GameObject[maxNumberOfAnimals];
     }
 
     public void addAnimalToArmy()
     {
-        if (playerOneArmy.Count < maxNumberOfAnimals)
+        if (countAnimals < maxNumberOfAnimals)
         {
-            GameObject animalSelected = EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject;
-            playerOneArmy.Add(animalSelected);
-            GameObject selectedAnimal = Instantiate(EventSystem.current.currentSelectedGameObject, Vector3.zero,
-                Quaternion.identity);
+            GameObject animalSelected = EventSystem.current.currentSelectedGameObject;
+            GameObject selectedAnimal = Instantiate(EventSystem.current.currentSelectedGameObject,
+                Vector3.zero, Quaternion.identity);
 
             selectedAnimal.GetComponent<Button>().onClick.AddListener(deleteAnimalFromArmy);
-            //selectedAnimal.GetComponent<Button>().onClick.SetPersistentListenerState
             UnityEventTools.RemovePersistentListener(selectedAnimal.GetComponent<Button>().onClick, 0);
             
             foreach (Transform animalPos in PlayerOneArmyPosition.transform)
             {
                 if (animalPos.childCount == 0) // dont have child
                 {
-                    Debug.Log(animalPos.transform);
-                    selectedAnimal.transform.parent = animalPos;
+                    selectedAnimal.transform.SetParent(animalPos);
                     selectedAnimal.transform.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                     selectedAnimal.transform.transform.localPosition = Vector3.zero;
+                    playerOneArmy[Int32.Parse(animalPos.name)] = animalSelected.transform.GetChild(0).gameObject;
+                    countAnimals++;
                     break;
                 }
             }
         }
-        
     }
 
     public void deleteAnimalFromArmy()
     {
-        playerOneArmy.Remove(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject);
+        int indexToDelete = Int32.Parse(EventSystem.current.currentSelectedGameObject.transform.parent.name);
+        playerOneArmy[indexToDelete] = null;
+        countAnimals--;
         Destroy(EventSystem.current.currentSelectedGameObject);
     }
 }
