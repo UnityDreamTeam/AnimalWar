fd62e0c72f7031018a112740561f7ff05bec50ca
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-01-08 17:57:44 +0200

    Update animal selection

--- Assets/Scripts/UI/AnimalsChoose.cs
+++ Assets/Scripts/UI/AnimalsChoose.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Events;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class AnimalsChoose : MonoBehaviour
 {
+    [SerializeField] GameObject PlayerOneArmyPosition;
+    List<GameObject> playerOneArmy;
+    readonly int maxNumberOfAnimals = 5;
+    readonly int minNumberOfAnimals = 1;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerOneArmy = new List<GameObject>();
+    }
+
+    public void addAnimalToArmy()
+    {
+        if (playerOneArmy.Count < maxNumberOfAnimals)
+        {
+            GameObject animalSelected = EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject;
+            playerOneArmy.Add(animalSelected);
+            GameObject selectedAnimal = Instantiate(EventSystem.current.currentSelectedGameObject, Vector3.zero,
+                Quaternion.identity);
+
+            selectedAnimal.GetComponent<Button>().onClick.AddListener(deleteAnimalFromArmy);
+            //selectedAnimal.GetComponent<Button>().onClick.SetPersistentListenerState
+            UnityEventTools.RemovePersistentListener(selectedAnimal.GetComponent<Button>().onClick, 0);
+            
+            foreach (Transform animalPos in PlayerOneArmyPosition.transform)
+            {
+                if (animalPos.childCount == 0) // dont have child
+                {
+                    Debug.Log(animalPos.transform);
+                    selectedAnimal.transform.parent = animalPos;
+                    selectedAnimal.transform.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    selectedAnimal.transform.transform.localPosition = Vector3.zero;
+                    break;
+                }
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void deleteAnimalFromArmy()
     {
-        
+        playerOneArmy.Remove(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject);
+        Destroy(EventSystem.current.currentSelectedGameObject);
     }
 }
--- Assets/Scripts/UI/MapChoose.cs
+++ Assets/Scripts/UI/MapChoose.cs
@@ -7,26 +7,20 @@ using UnityEngine.UI;
 
 public class MapChoose : MonoBehaviour
 {
-    [SerializeField] Button[] maps = new Button[3];
-    
+    [SerializeField] static int numOfMaps;
+    [SerializeField] Button[] maps = new Button[numOfMaps];
     private int currentMap;
     readonly int lowerBoundary = -1;
     readonly int higherBoundary = 3;
 
-    [SerializeField] GameObject PlayerOneArmyPosition;
-    List<GameObject> playerOneArmy;
-    
-
     // Start is called before the first frame update
     private void Start()
-	{
+    {
         currentMap = 0;
         transform.GetChild(currentMap).gameObject.SetActive(true);
-        playerOneArmy = new List<GameObject>();
-
     }
 
-	void selectMap(int index)
+    void selectMap(int index)
     {
         for (int i = 0; i < transform.childCount; i++)
         {
@@ -46,32 +40,5 @@ public class MapChoose : MonoBehaviour
     public void startGame()
     {
         SceneManager.LoadScene(currentMap + 2);
-    }
-
-    public void addAnimalToArmy()
-    {
-        GameObject animalSelected = EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject;
-        playerOneArmy.Add(animalSelected);
-        //Debug.Log(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject.name + "");
-        GameObject selectedAnimal = Instantiate(EventSystem.current.currentSelectedGameObject, Vector3.zero,
-            Quaternion.identity);
-        selectedAnimal.transform.GetChild(0).transform.localScale = Vector3.one;
-
-        foreach (Transform animalPos in PlayerOneArmyPosition.transform)
-        {
-            if (animalPos.childCount == 0) // dont have child
-            {
-                Debug.Log(animalPos.transform);
-                selectedAnimal.transform.parent = animalPos;
-                //selectedAnimal.transform.localPosition = animalPos.localPosition;
-                break;
-            }
-        }
-        
-    }
-
-    public void deleteAnimalFromArmy()
-    {
-        playerOneArmy.Remove(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject);
     }
 }
