3fc155c2dda14072b33570697905299d8910d79d
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-01-10 23:53:00 +0200

    Fix winning mechanism

--- Assets/Scripts/GameManager/VictoryChecker.cs
+++ Assets/Scripts/GameManager/VictoryChecker.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class VictoryChecker : MonoBehaviour
 {
+    [SerializeField] TextMeshPro team1Win = null;
+    [SerializeField] TextMeshPro team2Win = null;
+    [SerializeField] GameObject timer;
+    [SerializeField] GameObject battle_system;
     GameObject[] teamOne;
     GameObject[] teamTwo;
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
         teamOne = GameObject.FindGameObjectsWithTag("PlayerOne");
         teamTwo = GameObject.FindGameObjectsWithTag("PlayerTwo");
@@ -16,11 +21,20 @@ public class VictoryChecker : MonoBehaviour
         if (teamOne.Length == 0)
         {
             Debug.Log("Team 2 wins");
+            team2Win.enabled = true;
+            killProcesses();
         }
         else if (teamTwo.Length == 0)
         {
             Debug.Log("Team 1 wins");
-        }
- 
+            team1Win.enabled = true;
+            killProcesses();
+        } 
+    }
+
+    void killProcesses()
+    {
+        Destroy(battle_system);
+        Destroy(timer);
     }
 }
