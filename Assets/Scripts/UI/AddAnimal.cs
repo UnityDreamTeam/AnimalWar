4386924eda2bfb2b1d2d6026587cff226924b8d8
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2021-01-10 23:01:41 +0200

    Fix errors

---
+++ Assets/Scripts/UI/AddAnimal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddAnimal : MonoBehaviour
+{
+    Button myButton;
+    [SerializeField] GameObject animalChooser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+        myButton.onClick.AddListener(animalChooser.GetComponent<AnimalsChoose>().addAnimalToArmy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
