3ea75c30814dec4835884029d0d899bb74a11e9b
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2021-01-12 01:37:40 +0200

    Add button continue to regame

--- Assets/Scripts/GameManager/VictoryChecker.cs
+++ Assets/Scripts/GameManager/VictoryChecker.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryChecker : MonoBehaviour
 {
@@ -9,13 +8,18 @@ public class VictoryChecker : MonoBehaviour
     [SerializeField] TextMeshPro team2Win = null;
     [SerializeField] GameObject timer = null;
     [SerializeField] GameObject battle_system = null;
+    [SerializeField] GameObject continueBtn = null;
     GameObject teamOne;
     GameObject teamTwo;
+    GameObject animalChoose = null;
+    readonly int menuScene = 0;
 
     void Start()
     {
         teamOne = GameObject.Find("ArmyALocation");
         teamTwo = GameObject.Find("ArmyBLocation");
+        animalChoose = GameObject.Find("AnimalChooser");
+
     }
         // Update is called once per frame
     void Update()
@@ -23,19 +27,28 @@ public class VictoryChecker : MonoBehaviour
         if (teamOne.transform.childCount == 0)
         {
             team2Win.enabled = true;
+            continueBtn.SetActive(true);
             killProcesses();
         }
         else if (teamTwo.transform.childCount == 0)
         {
-
             team1Win.enabled = true;
+            continueBtn.SetActive(true);
             killProcesses();
         } 
     }
+
+    public void continueClick()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+
+
 
     void killProcesses()
     {
         Destroy(battle_system);
         Destroy(timer);
+        Destroy(animalChoose);
     }
 }
